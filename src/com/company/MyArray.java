192cab999ac9b4605fb7e0b0bb53702db9520b75
pavel-malin <kurchevskijpavel@gmail.com>
2020-02-24 13:50:19 +0300

    new files Array, Array_1, Interfaces, MyArray

---
+++ src/com/company/MyArray.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class MyArray implements Array {
+    int a[] = new int[50];
+    int size = 0;
+
+    @Override
+    public int Get (int i) {
+        return a[i];
+    }
+
+    @Override
+    public boolean Add (int val) {
+        if (size != a.length) {
+            a[size] = val;
+            size++;
+            return true;
+        } else
+            return false;
+    }
+}
