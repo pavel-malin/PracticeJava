e82513d817513345a78781dec9e9ff7303f89160
pavel-malin <kurchevskijpavel@gmail.com>
2020-02-24 11:43:23 +0300

    new files Circle, Polymorphism, Shape, Triangle

---
+++ src/com/company/Polymorphism.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Shape[] arr = new Shape[] {new Circle(), new Square(), new Triangle()};
+        for (int i = 0; i < arr.length; i++)
+            arr[i].draw();
+    }
+}
